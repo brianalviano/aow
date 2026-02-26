3ad3db0c65810a80c745d930cc3747227282db57
Brian Alviano <brianalviano.dev@gmail.com>
2026-02-26 23:54:16 +0700

    feat: add initial CompanyProfile seeder with default company information.

--- database/seeders/CompanyProfileSeeder.php
+++ database/seeders/CompanyProfileSeeder.php
@@ -13,14 +13,14 @@ class CompanyProfileSeeder extends Seeder
     public function run(): void
     {
         CompanyProfile::create([
-            'name'      => 'AOW Order',
-            'email'     => 'aow-order@gmail.com',
+            'name'      => 'AOWenak',
+            'email'     => 'aowenak@gmail.com',
             'phone'     => '082335032821',
             'whatsapp'  => '6282335032821',
             'address'   => 'Jl. Medokan Asri Utara IX No.25, Medokan Ayu, Kec. Rungkut, Surabaya, Jawa Timur 60295',
-            'instagram' => 'https://instagram.com/aow',
-            'facebook'  => 'https://facebook.com/aow',
-            'tiktok'    => 'https://tiktok.com/@aow',
+            'instagram' => 'https://instagram.com/aowenak',
+            'facebook'  => 'https://facebook.com/aowenak',
+            'tiktok'    => 'https://tiktok.com/@aowenak',
         ]);
     }
 }
