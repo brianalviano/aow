053507040b9634b9d29ba438ddb15a51138645ae
Brian Alviano <brianalviano.dev@gmail.com>
2026-03-04 13:43:33 +0700

    feat: add DropPointCategory enum with labels, descriptions, and utility methods for drop point classification.

--- app/Enums/DropPointCategory.php
+++ app/Enums/DropPointCategory.php
@@ -13,9 +13,12 @@ enum DropPointCategory: string
     case OFFICE = 'office';
     case RESIDENTIAL = 'residential';
     case SHOP = 'shop';
-    case MOSQUE = 'mosque';
     case PUBLIC_SPACE = 'public_space';
     case HEALTHCARE = 'healthcare';
+    case UNIVERSITY = 'university';
+    case OFFICE_AREA = 'office_area';
+    case SHOP_AREA = 'shop_area';
+    case WORSHIP_PLACE = 'worship_place';
     case OTHER = 'other';
 
     /**
@@ -30,9 +33,12 @@ enum DropPointCategory: string
             self::OFFICE => 'Kantor',
             self::RESIDENTIAL => 'Perumahan',
             self::SHOP => 'Toko/Ruko',
-            self::MOSQUE => 'Masjid/Musholla',
             self::PUBLIC_SPACE => 'Area Publik',
             self::HEALTHCARE => 'Kesehatan',
+            self::UNIVERSITY => 'Kampus / Universitas',
+            self::OFFICE_AREA => 'Perkantoran',
+            self::SHOP_AREA => 'Pertokoan',
+            self::WORSHIP_PLACE => 'Peribadahan',
             self::OTHER => 'Lainnya',
         };
     }
@@ -47,9 +53,12 @@ enum DropPointCategory: string
             self::OFFICE => 'Titik jemput di area perkantoran atau instansi.',
             self::RESIDENTIAL => 'Titik jemput di area perumahan atau permukiman.',
             self::SHOP => 'Titik jemput di area pertokoan atau ruko.',
-            self::MOSQUE => 'Titik jemput di area masjid atau musholla.',
             self::PUBLIC_SPACE => 'Titik jemput di fasilitas umum atau taman.',
             self::HEALTHCARE => 'Titik jemput di area rumah sakit atau klinik.',
+            self::UNIVERSITY => 'Titik jemput di area kampus atau universitas.',
+            self::OFFICE_AREA => 'Titik jemput di kawasan perkantoran.',
+            self::SHOP_AREA => 'Titik jemput di kawasan pertokoan.',
+            self::WORSHIP_PLACE => 'Titik jemput di tempat peribadahan.',
             self::OTHER => 'Titik jemput kategori lainnya.',
         };
     }
